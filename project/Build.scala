f375399154ec54c3d525cc50f00886aa1b2730b3
s.hanfi <souhail.hanfi@neoxia.com>
2012-09-21 18:47:35 +0200

    added jets3t dependency

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,13 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+    "net.java.dev.jets3t" % "jets3t" % "0.9.0"
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    resolvers += "jets3t" at "http://www.jets3t.org/maven2"
+    
       // Add your own project settings here      
     )
-
 }
